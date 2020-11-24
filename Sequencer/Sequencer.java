46615c2b40ef782a7f57846e9c6876ac7fb3c6f1
JonathanFrey2704 <jfrey2704@gmail.com>
2020-11-24 13:09:02 -0500

    update sequencer.java

--- Sequencer/Sequencer.java
+++ Sequencer/Sequencer.java
@@ -1,32 +1,65 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.*;
-import java.util.HashMap;
+import java.util.Scanner;
 
 public class Sequencer {
 
     DatagramSocket sequencerSocket;
-    DatagramSocket validitySocket;
-    InetAddress address;
-    int sequencer_port =2000;
-    int sequence_number = 0;
-    HashMap<InetAddress, Integer> RM_adresses = new HashMap<>();
+
+    InetAddress localAddress;
+    InetAddress sequencerAddress;
+    InetAddress RM1Address;
+    InetAddress RM2Address;
+    InetAddress RM3Address;
+
+    int localPort;
+    int sequencerPort;
+    int RM1Port;
+    int RM2Port;
+    int RM3Port;
+
+    int sequence_number;
+
+    private void ReadAddresses() throws UnknownHostException, FileNotFoundException {
+        Scanner fScn = new Scanner(new File("addresses.txt"));
+        String data;
+
+        while( fScn.hasNextLine() ){
+            data = fScn.nextLine();
+
+            String[] token = data.split(",");
+            this.localAddress = InetAddress.getByName(token[0]);
+            this.localPort = Integer.parseInt(token[1]);
+
+            this.sequencerAddress = InetAddress.getByName(token[2]);
+            this.sequencerPort = Integer.parseInt(token[3]);
+
+            this.RM1Address = InetAddress.getByName(token[4]);
+            this.RM1Port = Integer.parseInt(token[5]);
+
+            this.RM2Address = InetAddress.getByName(token[6]);
+            this.RM2Port = Integer.parseInt(token[7]);
+
+            this.RM3Address = InetAddress.getByName(token[8]);
+            this.RM3Port = Integer.parseInt(token[9]);
+
+        }
+        fScn.close();
+    }
 
 
-    public Sequencer() throws UnknownHostException, SocketException {
+    public Sequencer() throws UnknownHostException, SocketException, FileNotFoundException {
 
-        this.address = InetAddress.getLocalHost();
-        this.sequencerSocket = new DatagramSocket(this.sequencer_port, this.address);
-        this.validitySocket = new DatagramSocket(30000, this.address);
-
-        RM_adresses.put(InetAddress.getByName(""),0);
-        RM_adresses.put(InetAddress.getByName(""),2);
-        RM_adresses.put(InetAddress.getByName(""),3);
-
+        ReadAddresses();
+        sequence_number = 0;
+        this.sequencerSocket = new DatagramSocket(this.sequencerPort, this.sequencerAddress);
     }
 
     public void run() throws IOException {
 
-        boolean not_received;
+        boolean not_received = true;
         byte[] buffer;
 
         while (true) {
@@ -45,18 +78,26 @@ public class Sequencer {
 
             while (not_received) {
 
-                for (InetAddress key : RM_adresses.keySet()) {
-                    DatagramPacket reply = new DatagramPacket(response.getBytes(), response.length(), key, RM_adresses.get(key));
-                    sequencerSocket.send(reply);
-                }
+                DatagramPacket request1 = new DatagramPacket(response.getBytes(), response.length(), RM1Address, RM1Port);
+                DatagramPacket request2 = new DatagramPacket(response.getBytes(), response.length(), RM2Address, RM2Port);
+                DatagramPacket request3 = new DatagramPacket(response.getBytes(), response.length(), RM3Address, RM3Port);
+                sequencerSocket.send(request1);
+                sequencerSocket.send(request2);
+                sequencerSocket.send(request3);
+
                 try{
-                    validitySocket.setSoTimeout(1000);
+                    sequencerSocket.setSoTimeout(1000);
                     byte[] buff = new byte[1000];
 
-                    DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
-                    validitySocket.receive(reply);
-                    String answer = new String(reply.getData());
-                    if (answer.toUpperCase().equals("RECEIVED")) {
+                    DatagramPacket reply1 = new DatagramPacket(buff, buff.length);
+                    DatagramPacket reply2 = new DatagramPacket(buff, buff.length);
+                    DatagramPacket reply3 = new DatagramPacket(buff, buff.length);
+
+                    sequencerSocket.receive(reply1);
+                    sequencerSocket.receive(reply2);
+                    sequencerSocket.receive(reply3);
+
+                    if (new String(reply1.getData()).toUpperCase().equals("RECEIVED") && new String(reply2.getData()).toUpperCase().equals("RECEIVED") && new String(reply3.getData()).toUpperCase().equals("RECEIVED")) {
                         not_received = false;
                     }
                 } catch (SocketTimeoutException e) {
@@ -65,5 +106,6 @@ public class Sequencer {
 
         }
     }
+
 
 }
